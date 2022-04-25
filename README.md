98abe36633f222692f7421ccb6257478c1b508fc
dodjian <dovdjianpro@gmail.com>
2022-04-25 15:10:16 +0200

    [+] volume wordpress create, to do: fix mariadb er

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Ressources:
 
 .RUN vs CMD vs ENTRY_POINT
 https://www.geeksforgeeks.org/difference-between-run-vs-cmd-vs-entrypoint-docker-commands/#:~:text=A%20CMD%20command%20is%20used,the%20last%20one%20gets%20executed.
+
+Rechercher -F -R flag php-fpm
--- srcs/requirements/mariadb/tools/launch_db.sh
+++ srcs/requirements/mariadb/tools/launch_db.sh
@@ -3,8 +3,8 @@ set -xve
 service mysql start
 
 echo "CREATE DATABASE dov;" | mysql
-echo "CREATE USER 'dovuser'@'localhost' IDENTIFIED BY 'lol';" | mysql
-echo "GRANT ALL PRIVILEGES ON dov.* TO 'dovuser'@'localhost';" | mysql
+echo "CREATE USER 'dovuser'@'%' IDENTIFIED BY 'lol';" | mysql
+echo "GRANT ALL PRIVILEGES ON dov.* TO 'dovuser'@'%';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
 
 service mysql stop
