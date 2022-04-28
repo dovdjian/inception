0b937c1a73bd29e338dba519f5e16278977bc79a
Dov Djian <dovdjianpro@gmail.com>
2022-04-29 00:06:27 +0200

    [+] NOT WORKING, to do: try to not relaunch db

--- srcs/requirements/mariadb/tools/launch_db.sh
+++ srcs/requirements/mariadb/tools/launch_db.sh
@@ -1,11 +1,15 @@
 set -xve
 
 service mysql start
+DIR="/var/lib/mysql/mysql"
 
-echo "CREATE DATABASE IF NOT EXISTS dov;" | mysql
-echo "CREATE USER 'dovuser'@'%' IDENTIFIED BY 'lol';" | mysql
-echo "GRANT ALL PRIVILEGES ON dov.* TO 'dovuser'@'%';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-
+if [ -d "$DIR" ]; then
+	echo "'$DIR' found"
+else
+	echo "CREATE DATABASE dov;" | mysql
+	echo "CREATE USER 'dovuser'@'%' IDENTIFIED BY 'lol';" | mysql
+	echo "GRANT ALL PRIVILEGES ON dov.* TO 'dovuser'@'%';" | mysql
+	echo "FLUSH PRIVILEGES;" | mysql
+fi
 service mysql stop
 mysqld_safe
