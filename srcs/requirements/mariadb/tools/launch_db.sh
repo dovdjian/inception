651769095f6f7632496659a3fb9b87da959f7de4
dodjian <dovdjianpro@gmail.com>
2022-05-03 14:10:19 +0200

    [+] add root password

--- srcs/requirements/mariadb/tools/launch_db.sh
+++ srcs/requirements/mariadb/tools/launch_db.sh
@@ -1,11 +1,12 @@
 set -xve
 
-service mysql start
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql
-echo "CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USERNAME'@'%';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-
-service mysql stop
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	service mysql start
+	echo "CREATE DATABASE $DB_NAME;" | mysql
+	echo "CREATE USER '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
+	echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USERNAME'@'%';" | mysql
+	echo "FLUSH PRIVILEGES;" | mysql
+	echo "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql
+	service mysql stop
+fi
 mysqld_safe
